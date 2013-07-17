104c50faa8e576ea979db07f15afb8f0e7828b35
Heiko Dreyer <mail@boxedfolder.com>
2013-07-17 15:02:00 +0200

    init commit

---
+++ tests.rb
@@ -0,0 +1,20 @@
+require './sorter.rb'
+require 'test/unit'
+
+class TestCaseSorter < Test::Unit::TestCase
+  def test_bubble_sort
+    assert_equal(Sort.bubble_sort([5,2,5,65,10,4,2,8,2,3,1]), [1,2,2,2,3,4,5,5,8,10,65])
+  end
+
+  def test_quick_sort
+    assert_equal(Sort.quick_sort([5,2,5,65,10,4,2,8,2,3,1]), [1,2,2,2,3,4,5,5,8,10,65])
+  end
+  
+  def test_gnome_sort
+     assert_equal(Sort.gnome_sort([5,2,5,65,10,4,2,8,2,3,1]), [1,2,2,2,3,4,5,5,8,10,65])
+   end
+   
+   def test_insertion_sort
+      assert_equal(Sort.insertion_sort([5,2,5,65,10,4,2,8,2,3,1]), [1,2,2,2,3,4,5,5,8,10,65])
+    end
+end
